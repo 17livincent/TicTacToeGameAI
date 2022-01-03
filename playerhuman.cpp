6a8aff6c32afa77a745063b9d8e933a4feaf18d8
Vincent Li <li.vincent0@gmail.com>
2022-01-02 21:13:20 -0800

    Cleaner min verbose output for hp

--- playerhuman.cpp
+++ playerhuman.cpp
@@ -12,15 +12,17 @@
 moveRCPair HumanPlayer::chooseMove(Game* game) {
     std::string input;
     do {
+#if defined(VERBOSE) || defined(DEBUG)
         std::cout << "\t" << this->mark << ":"<< "Enter a coordinate of the form <row>,<col>: ";
+#elif defined(MINIMAL_VERBOSE)
+        std::cout << game->turns << " " << this->mark << ":";
+#endif
         std::cin >> input;
     } while(game->validatePlayerInput(input));
 
     moveRCPair move = processPlayerInput(input);
 #if defined(VERBOSE) || defined(DEBUG)
     std::cout << "\tPlayer " << this->mark << " marks " << move.row << "," << move.column << std::endl; 
-#elif defined(MINIMAL_VERBOSE)
-    std::cout << game->turns << " " << this->mark << ":" << move.row << "," << move.column << std::endl; 
 #endif
     return move;
 }
